9e7cd7e59ef651656eed677b8d6ef70e3906eefe
BramboraSK <30873527+BramboraSK@users.noreply.github.com>
2020-03-01 12:47:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('commandSearch');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("commands");
+    li = ul.getElementsByTagName('div');
+  
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function retheme() {
+    var cc = document.body.className;
+    if (cc.indexOf("darktheme") > -1) {
+        document.body.className = cc.replace("darktheme", "");
+        document.getElementById("dlbtn").src = "/static/images/sun.png"
+    } else {
+        document.body.className += " darktheme";
+        document.getElementById("dlbtn").src = "/static/images/moon.png"
+    }
+}
+
+function logged() {
+    var lb = document.getElementById("logbutton");
+    lb.innerHTML = "{{ logged }}";
+}
+
+function clicked(clickid) {
+    var x = document.getElementById(clickid+"n");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showcontent() {
+    var x = document.getElementById("dropcont");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function closecontent() {
+    var x = document.getElementById("dropcont");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+}
